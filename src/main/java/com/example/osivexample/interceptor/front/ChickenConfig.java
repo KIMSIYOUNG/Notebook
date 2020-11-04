be00b3ffecd38f831591e28ada49a301e1042e13
KIMSIYOUNG <tldud2404@gmail.com>
2020-11-04 16:03:22 +0900

    feat : osiv filter register

--- src/main/java/com/example/osivexample/interceptor/front/ChickenConfig.java
+++ src/main/java/com/example/osivexample/interceptor/front/ChickenConfig.java
@@ -2,7 +2,14 @@ package com.example.osivexample.interceptor.front;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.engine.spi.SessionFactoryDelegatingImpl;
+import org.hibernate.internal.SessionFactoryImpl;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.support.OpenSessionInViewFilter;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,5 +30,14 @@ public class ChickenConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(methodArgumentResolver);
+    }
+
+    @Bean
+    public FilterRegistrationBean registerOpenEntityManagerInViewFilterBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        OpenEntityManagerInViewFilter filter = new OpenEntityManagerInViewFilter();
+        registrationBean.setFilter(filter);
+        registrationBean.setOrder(5);
+        return registrationBean;
     }
 }
